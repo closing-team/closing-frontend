de7b9df1e78fccc4470ac632822769cfc80ebc96
yeop1108 <yeop1108@naver.com>
2026-07-03 23:56:25 +0900

    chore: 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-[#6558FF]">Closing</h1>
+    </div>
+  );
+}
+
+export default App;
